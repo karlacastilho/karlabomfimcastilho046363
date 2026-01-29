e236f2a5c800df05a99389a87b62b76cbf50e1b8
Karla Bomfim Castilho <04636379179@seguranca.local>
2026-01-29 15:24:49 -0400

    Initial commit

---
+++ src/main/java/com/karlacastilho/apiseletivo/ApiseletivoApplication.java
@@ -0,0 +1,13 @@
+package com.karlacastilho.apiseletivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiseletivoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiseletivoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/karlacastilho/apiseletivo/ApiseletivoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karlacastilho.apiseletivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiseletivoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
